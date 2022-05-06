5cb508a711062644e24f70f30da184f9409d4dcf
hoosg <hushige.bisu@gmail.com>
2022-05-06 22:27:47 +0200

    3DP

---
+++ story_content/user.js
@@ -0,0 +1,178 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6FVAp27Z4OU":
+        Script1();
+        break;
+      case "6oFCnYD4SLR":
+        Script2();
+        break;
+      case "5iksUY9gcLR":
+        Script3();
+        break;
+      case "6hNdFPA7Uky":
+        Script4();
+        break;
+      case "6qso16Jes3h":
+        Script5();
+        break;
+      case "5rtPFPhVWR6":
+        Script6();
+        break;
+      case "62I4W2syvSE":
+        Script7();
+        break;
+      case "5fbKrleK4OE":
+        Script8();
+        break;
+      case "6JtE2QWikIi":
+        Script9();
+        break;
+      case "5c63XbBBlDe":
+        Script10();
+        break;
+      case "5vh8MMOf1A0":
+        Script11();
+        break;
+      case "5tcYTPwG0g1":
+        Script12();
+        break;
+      case "6E10vSlmtWo":
+        Script13();
+        break;
+      case "6liSdjQfgHT":
+        Script14();
+        break;
+      case "6BaoiNewLWq":
+        Script15();
+        break;
+      case "6IgtLBNsICR":
+        Script16();
+        break;
+      case "6FEcW7eHMGx":
+        Script17();
+        break;
+      case "5wkRQkH7STq":
+        Script18();
+        break;
+      case "6UIcrumEXtv":
+        Script19();
+        break;
+  }
+}
+
+function Script1()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script2()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script3()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script4()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script5()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script6()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script7()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script8()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script9()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script10()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script11()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script12()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script13()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script14()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script15()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script16()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script17()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script18()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNum",number+1);
+}
+
+function Script19()
+{
+  var number=Math.floor(Math.random()*17);
+GetPlayer().SetVar("RandNumAdj",number+1);
+}
+
